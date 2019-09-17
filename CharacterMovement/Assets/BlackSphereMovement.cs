d0ff4ee7a6a0db5a0ea61587cd0c8f47e26f0669
Patrick Rufino <patrick.rufino@gmail.com>
2019-09-17 11:50:45 -0400

    Made three shapes, 2 moving by themselves and 1 moving with user input

---
+++ CharacterMovement/Assets/BlackSphereMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackSphereMovement : MonoBehaviour
+{   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 10)
+        {
+            transform.position = new Vector3(-5, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(0, 3 * Time.deltaTime, 0);
+        }
+    }
+
+    }
+
---
+++ CharacterMovement/Assets/RectangleMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectangleMovement : MonoBehaviour
+{
+    public float EngineStart;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            EngineStart = 2;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-3 * Time.deltaTime * EngineStart, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(6 * Time.deltaTime * EngineStart, 0, 0);
+        }
+
+
+    }
+}
